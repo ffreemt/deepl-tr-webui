46f260fc251fbbe2102e3f687582f7e1f170671f
ffreemt <yucongo+fmt@gmail.com>
2023-01-20 12:38:48 +0800

    Update httpserver with cors header (Access-Control-Allow-Origin)

--- deepl_tr/httpserver.py
+++ deepl_tr/httpserver.py
@@ -15,7 +15,7 @@ class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         http.server.SimpleHTTPRequestHandler.end_headers(self)
 
     def send_my_headers(self):
-        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Origin", "*")  # cors header
 
 
 def do_GET(self):
